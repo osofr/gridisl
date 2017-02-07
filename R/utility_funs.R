be93f054e9e2fae6647693ab4cb973be8036ae50
Oleg Sofrygin <olegso@gmail.com>
2017-02-07 14:39:17 -0800

    export import

--- R/utility_funs.R
+++ R/utility_funs.R
@@ -132,27 +132,20 @@ add_holdout_ind = function(data, ID, hold_column = "hold", random = TRUE, seed =
   return(data)
 }
 
-# ---------------------------------------------------------------------------------------
-# Import data, define nodes (columns), define dummies for factor columns and define input data R6 object
-#
-# @param data Input dataset, can be a \code{data.frame} or a \code{data.table}.
-# @param ID A character string name of the column that contains the unique subject identifiers.
-# @param t_name A character string name of the column with integer-valued measurement time-points (in days, weeks, months, etc).
-# @param covars Names of predictors (covariates) in the data.
-# @param OUTCOME Character name of the column containing outcomes.
-# @param verbose Set to \code{TRUE} to print messages on status and information to the console. Turn this on by default using \code{options(gridisl.verbose=TRUE)}.
-# @return An R6 object that contains the input data. This can be passed as an argument to \code{get_fit} function.
-# @example tests/examples/1_gridisl_example.R
-# @export
-
+#' ---------------------------------------------------------------------------------------
+#' Import data, define nodes (columns), define dummies for factor columns and define input data R6 object
+#'
+#' @param data Input dataset, can be a \code{data.frame} or a \code{data.table}.
+#' @param ID A character string name of the column that contains the unique subject identifiers.
+#' @param t_name A character string name of the column with integer-valued measurement time-points (in days, weeks, months, etc).
+#' @param covars Names of predictors (covariates) in the data.
+#' @param OUTCOME Character name of the column containing outcomes.
+#' @param verbose Set to \code{TRUE} to print messages on status and information to the console. Turn this on by default using \code{options(gridisl.verbose=TRUE)}.
+#' @return An R6 object that contains the input data. This can be passed as an argument to \code{get_fit} function.
+#' @example tests/examples/1_gridisl_example.R
+#' @export
 importData <- function(data, ID = "Subject_ID", t_name = "time_period", covars, OUTCOME = "Y", verbose = getOption("gridisl.verbose")) {
   gvars$verbose <- verbose
-  # if (verbose) {
-  #   current.options <- utils::capture.output(str(gvars$opts))
-  #   print("Using the following gridisl options/settings: ")
-  #   cat('\n')
-  #   cat(paste0(current.options, collapse = '\n'), '\n')
-  # }
 
   if (missing(covars)) { # define time-varing covars (L) as everything else in data besides these vars
     covars <- setdiff(colnames(data), c(ID, OUTCOME))
@@ -161,43 +154,6 @@ importData <- function(data, ID = "Subject_ID", t_name = "time_period", covars, 
   nodes <- list(Lnodes = covars, Ynode = OUTCOME, IDnode = ID, tnode = t_name)
   OData <- DataStorageClass$new(Odata = data, nodes = nodes)
 
-  ## --------------------------------------------------------------------------------------------------------
-  ## Convert all character covars into factors?
-  ## --------------------------------------------------------------------------------------------------------
-  # ....?
-
-  ## --------------------------------------------------------------------------------------------------------
-  ## Create dummies for each factor
-  ## --------------------------------------------------------------------------------------------------------
-  # factor.Ls <- unlist(lapply(OData$dat.sVar, is.factor))
-  # factor.Ls <- factor.Ls[covars]
-  # factor.Ls <- names(factor.Ls)[factor.Ls]
-
-  # new.factor.names <- vector(mode="list", length=length(factor.Ls))
-  # names(new.factor.names) <- factor.Ls
-  # if (length(factor.Ls)>0 && verbose)
-  #   message("...converting the following factor(s) to binary dummies (and droping the first factor levels): " %+% paste0(factor.Ls, collapse=","))
-  # for (factor.varnm in factor.Ls) {
-  #   factor.levs <- levels(OData$dat.sVar[,factor.varnm, with=FALSE][[1]])
-  #   factor.levs <- factor.levs[-1] # remove the first level (reference class)
-  #   # use levels to define cat indicators:
-  #   OData$dat.sVar[,(factor.varnm %+% "_" %+% factor.levs) := lapply(factor.levs, function(x) levels(get(factor.varnm))[get(factor.varnm)] %in% x)]
-  #   # to remove the origional factor var: # OData$dat.sVar[,(factor.varnm):=NULL]
-  #   new.factor.names[[factor.varnm]] <- factor.varnm %+% "_" %+% factor.levs
-  # }
-  # OData$new.factor.names <- new.factor.names
-
-  ## --------------------------------------------------------------------------------------------------------
-  ## Convert all logical vars to binary integers
-  ## --------------------------------------------------------------------------------------------------------
-  # logical.Ls <- unlist(lapply(OData$dat.sVar, is.logical))
-  # logical.Ls <- names(logical.Ls)[logical.Ls]
-  # if (length(logical.Ls)>0 && verbose) message("...converting logical columns to binary integers (0 = FALSE)...")
-  # for (logical.varnm in logical.Ls) {
-  #   OData$dat.sVar[,(logical.varnm) := as.integer(get(logical.varnm))]
-  # }
-
-  # for (Ynode in nodes$Ynode)  CheckVarNameExists(OData$dat.sVar, Ynode)
   for (Lnode in nodes$Lnodes) CheckVarNameExists(OData$dat.sVar, Lnode)
   return(OData)
 }
